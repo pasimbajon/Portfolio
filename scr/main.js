58428e93ec5eabdcbb3e2b9654be8dd483de6a14
PASims <40814631+pasimbajon@users.noreply.github.com>
2024-03-20 15:53:05 +0800

    first commit

---
+++ scr/main.js
@@ -0,0 +1,11 @@
+import '../styles/modern-normalize.css'
+import '../styles/style.css'
+import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
+import '../styles/components/mobile-nav.css'
+import '../styles/utilities.css'
+
+import mobileNav from './utilities/mobile-nav';
+mobileNav();
+import switchTheme from './utilities/dark-theme';
---
+++ scr/utilities/dark-theme.js
@@ -0,0 +1,21 @@
+//Switch funtion
+const switchTheme= () => {
+    //Get the root element and data-theme value
+    const rootElement = document.documentElement
+
+    let dataTheme = rootElement.getAttribute('data-theme'),
+        newTheme;
+
+    newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+    //Set the new HTML attribute
+    rootElement.setAttribute('data-theme', newTheme);
+
+    //Set the new local storage item
+    localStorage.setItem('theme', newTheme);
+};
+//Add eventlistiner fotr the theme switcher
+document.querySelector('#header__theme-switcher').addEventListener('click', switchTheme);
+document.querySelector('#mobile__theme-switcher').addEventListener('click', switchTheme);
+
+export default switchTheme;
