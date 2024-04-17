247f39dd153a5a804ae337f14d185eb71a99bce6
PASims <40814631+pasimbajon@users.noreply.github.com>
2024-04-17 20:32:07 +0800

    Working Contact form

--- scr/main.js
+++ scr/main.js
@@ -6,6 +6,7 @@ import '../styles/components/about.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/components/project.css'
 import '../styles/components/contact.css'
+import '../styles/components/footer.css'
 import '../styles/utils.css'
 
 import mobileNav from './utils/mobile-nav';
